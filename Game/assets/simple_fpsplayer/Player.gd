c6b803f6cfba77cd8173cf6f2fb9867051212fe5
Max Lowrey <maxlow29@kingshigh.school.nz>
2024-06-26 11:00:52 +1200

    Controls
    - Rebounded controls to WASD and not arrows (Works better with space)
    - Fixed modeling errors and added doorframes in blender, door open animations to be added next period

--- Game/assets/simple_fpsplayer/Player.gd
+++ Game/assets/simple_fpsplayer/Player.gd
@@ -5,7 +5,7 @@ const DEACCEL = 30
 
 const SPEED = 5.0
 const SPRINT_MULT = 2
-const JUMP_VELOCITY = 4.5
+const JUMP_VELOCITY = 6.5
 const MOUSE_SENSITIVITY = 0.06
 
 # Get the gravity from the project settings to be synced with RigidDynamicBody nodes.
@@ -71,7 +71,7 @@ func _physics_process(delta):
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with a custom keymap depending on your control scheme. These strings default to the arrow keys layout.
-	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var input_dir = Input.get_vector ("left","right","up","down")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized() * accel * delta
 	if Input.is_key_pressed(KEY_SHIFT):
 		direction = direction * SPRINT_MULT
